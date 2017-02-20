6722627296a5c08d79ec5e238bba600c4d767498
Sena <love-diving@hotmail.com>
2017-02-20 20:27:54 +1300

    map included

---
+++ js/basic.js
@@ -0,0 +1,49 @@
+var APPID = "5b5679716bb98722f6ef49d25de425f6";
+var temp;
+var loc;
+var icon;
+var humidity;
+var wind;
+var direction;
+var APIkey = "AIzaSyDHw_6CQTPkYWZe0A5UqFRKME4pZRjBi1A";
+
+function update(weather) {
+    alert(weather.description);
+    document.getElementById("description").innerHTML = weather.description;
+    document.getElementById("humidity").innerHTML = weather.humidity;
+    document.getElementById("wind").innerHTML = weather.wind;
+    document.getElementById("direction").innerHTML = weather.direction;
+    document.getElementById("location").innerHTML = weather.location;
+    document.getElementById("temperature").innerHTML = weather.temp;
+}
+
+function updateByCityName(location) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?" +
+        "q=" + location +
+        "&APPID=" + APPID;
+    sendRequest(url);
+}
+
+function geolocationMap() {
+    var url="https://maps.googleapis.com/maps/api/js?" +
+    "key=" + APIkey
+    "&callback=initMap";
+    
+    sendRequest(url);
+}
+
+function initMap() {
+       
+  var myLatLng = {lat: -25.363, lng: 131.044};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
