47d80412b61e6b834683d6697f8ccd1acffed9c6
ImagineCura <caregivercura@gmail.com>
2017-03-15 21:45:43 +1300

    Update data files for scheduler 15/03/2015

--- samples/01_initialization_loading/data/events.php
+++ samples/01_initialization_loading/data/events.php
@@ -2,6 +2,6 @@
 	require_once('../../common/connector/scheduler_connector.php');
 	include ('../../common/config.php');
 
-	$scheduler = new JSONSchedulerConnector($res, $dbtype);
+	$scheduler = new schedulerConnector($res, $dbtype);
 	$scheduler->render_table("events","event_id","start_date,end_date,event_name,details");
 ?>
